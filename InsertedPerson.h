dc77a8ab93d2a8c4e96d7a8c40ef4aab04c75082
Matthew Raymond <matthewraymond@Matthews-MacBook-Pro.local>
2019-04-24 14:22:21 -0700

    Added insert person functionality

---
+++ InsertedPerson.h
@@ -0,0 +1,41 @@
+#pragma once
+
+#include <string>
+
+#include "Manips.h"
+#include "BST.h"
+#include "Person.h"
+
+class InsertedPerson: public Manips
+{
+public:
+    InsertedPerson();
+    InsertedPerson(Person myP, BST<Person>* aT);
+    void undoOperation(); // Delete
+    void redoOperation();
+
+private:
+    Person affectedPerson;
+    BST<Person>* affectedTree;
+};
+
+InsertedPerson::InsertedPerson()
+{
+
+}
+
+InsertedPerson::InsertedPerson(Person myP, BST<Person>* aT)
+    :affectedPerson(myP), affectedTree(aT)
+{
+
+}
+
+void InsertedPerson::undoOperation() // Oposite of action done
+{
+    affectedTree->deleteR(affectedPerson.id);
+}
+
+void InsertedPerson::redoOperation() // Action done
+{
+    affectedTree->insert(affectedPerson.id, affectedPerson);
+}
