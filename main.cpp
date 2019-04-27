d2a8ab147a65d6937e68b2c2aa2ea54e33aae536
Matthew Raymond <matthewraymond@Matthews-MacBook-Pro.local>
2019-04-26 19:23:41 -0700

    Commented out extra stuff for now

--- main.cpp
+++ main.cpp
@@ -17,41 +17,26 @@ int main(int argc, char const *argv[])
     //Fixed by passing by reference!
     BST<Person*> myTree;
 
-    Student s(10,"Matt","Freshman","Music",11,4.0);
-    Student q(100,"Joe","Freshman","Music",11,4.0);
-    Student l(101,"Helen","Freshman","Music",11,4.0);
-
-    Faculty f(12,"Mandrew","associate","math");
-    f.AddAvisee(10);
-
-    InsertedPerson myPerson(&s, &myTree);
-    InsertedPerson myPerson1(&q, &myTree);
-    InsertedPerson myPerson2(&l, &myTree);
-    // DeletedPerson myPerson2(f, &myTree);
-
-    myPerson.redoOperation();
-    myPerson1.redoOperation();
-    myPerson2.redoOperation();
-
-    cout << "----------" << endl;
-    myTree.printTree();
-
-
-    // myPerson2.undoOperation();
-
-    serializer myS;
-    myS.serializeTree(myTree);
+    // Student s(10,"Matt","Freshman","Music",11,4.0);
+    // Student q(100,"Joe","Freshman","Music",11,4.0);
+    // Student l(101,"Helen","Freshman","Music",11,4.0);
     //
-    // myTree.printTree();
+    // Faculty f(12,"Mandrew","associate","math");
+    // f.AddAvisee(10);
     //
-    // myPerson.undoOperation();
+    // InsertedPerson myPerson(&s, &myTree);
+    // InsertedPerson myPerson1(&q, &myTree);
+    // InsertedPerson myPerson2(&l, &myTree);
+    //
+    // myPerson.redoOperation();
+    // myPerson1.redoOperation();
     // myPerson2.redoOperation();
     //
     // cout << "----------" << endl;
     // myTree.printTree();
-
-
-    GenStack<Manips> myStudents(5);
+    //
+    // serializer myS;
+    // myS.serializeTree(myTree);
 
 
     return 0;
